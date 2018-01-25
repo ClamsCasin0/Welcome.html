689edbbe89ac2fd631b64b589ee36f42a1642ce0
armandosegura <segura_arm@yahoo.com>
2018-01-25 16:47:22 -0600

    inline,external_html and external.js

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+    // console.log("This is coming from the external.js file");
+    //     var favoriteColor = "blue";
+    //         console.log("My favorite color is " favoriteColor )
+    alert("Welcome to my Website!");
+
+    var favoriteNumber = prompt("What is your color?");
+        console.log("My favorite color is " favoriteColor);
+        alert("Rad, blue is my favorite color too!");

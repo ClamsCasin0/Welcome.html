fda78083c5b3797d98c073d3561356098da9b595
armandosegura <segura_arm@yahoo.com>
2018-02-01 11:19:02 -0600

    added bonus

--- JS/planets-string.js
+++ JS/planets-string.js
@@ -20,7 +20,7 @@
      * results. Why might this be useful?
      */
     //
-    var planetsBrString = planetsArray.join('<br>');
+    var planetsBreakString = planetsArray.join('<br>');
     console.log(planetsBrString);
 
 
@@ -32,4 +32,54 @@
      * string, and <li> tags around each planet.
      */
 
+     var unorderedPlanets = planetsArray.join('</li><li>');
+
+     var listedPlanets = unorderedPlanets.split(' ');
+
+     listedPlanets.push('</li></ul>');
+
+     listedPlanets.unshift('<ul><li>');
+
+     listedPlanets.join(' ');
+
+     console.log(listedPlanets);
+
+     document.write(listedPlanets);
+
+
+
+    // var planetsUnorderedString = planetsArray.unshift('<ul>');
+    // console.log(planetsUnorderedString);
+    // var planetsUnorderedString = planetsArray.push('</ul>');
+//
+//
+// function planetsUlString(stringer) {
+
+
+================================= ARRAY MANIPULATION
+    /** BONUS 1:
+    Create a phone number parser that will convert a string of numbers into a human readable format. The output should depend on the number of digits (account for 7, 10, 11 digit numbers):
+    Using multiple functions will be helpful.
+        input = 5552324343, output = 555-232-4343
+    input = 5553434, output = 555-3434
+    input = 18005552323, output = 1-800-555-2323
+    */
+
+    output10 = input.
+
+    /**
+    Extra Challenge: account for invalid characters
+    Extra Challenge: if the input is already formatted, output the unformatted version
+    Extra Challenge: allow the parser to accept letters and convert them to the correct numbers
+    BONUS 2:
+    Create a student roster management app. Using the user dialogue functions (alert, confirm, prompt), create an application that will prompt the user to choose from a menu with the following actions:
+        1) Add a student name
+    2) Delete a student name
+    3) View all student names in alphabetical order
+    4) View all student names in reverse alphabetical order
+    Store the student names in an array. Account for invalid user input.
+    */
+
+
+}
 })();

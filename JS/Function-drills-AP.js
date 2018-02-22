d6d4e76dcf1d45bdfc0df3d01e6488ad408ed5c1
armandosegura <segura_arm@yahoo.com>
2018-02-22 10:07:55 -0600

    small changes on Promises

---
+++ JS/Function-drills-AP.js
@@ -0,0 +1,69 @@
+"use stricy";
+
+
+// Make a function named sayHello(name) that returns "Hi " + name.
+function sayHello(name) {
+    return "Hi" + name + "!";
+}
+console.log(sayHello("AJ"));
+
+//     Write a function named capitalize(string) that takes a string of
+// any capitalization and uppercases the first character and lowercases
+// the rest of the characters of a string.
+function capitalize(string) {
+
+}
+
+//     Write a function named upperCase(string)
+function upperCase(string) {
+
+}
+// Make a function named identity(input) that returns the input exactly as provided.
+function identity(input) {
+    return input;
+}
+//     Make a function named isOdd(number)
+function isOdd(number) {
+    number % 2 !== 0;
+}
+// Make a function named isEven(number)
+function isEven(number) {
+    return number % 2 == 0;
+}
+// Make a function named isPositive(number)
+// Make a function named isNegative(number)
+// Make a function named isBoolean(input)
+function isBoolean(input) {
+    typeof === "boolean";
+}
+// Make a function named isTrue(boolean)
+// Make a function named isFalse(boolean)
+// Make a function named isTruthy(input)
+// Make a function named isFalsy(input)
+// Make a function named isVowel(letter)
+// Make a function named hasVowels(string) that uses isVowel to check if a string has any vowels
+// Make a function named isConsonant(letter)
+// Make a function named hasConsonants(string)
+// Make a function named isCapital(letter)
+// Make a function named hasCapitalLetters(string) that returns if a string has any capitals.
+//     Make a function named isLowerCase(letter)
+// Make a function named hasLowerCase(string) that returns if a string has any lower cased
+// Make a function named isSpace(letter) that returns if a character is a space character
+// Make a function named hasSpaces(string) that returns if a string has any space characters
+// Make a function named isZero(number)
+// Make a function named increment(number) that returns a number plus one
+// Make a function named decrement(number) that returns a number minus one
+// Make a function named isFive(input)
+// Make a function named addFive(input) that adds five to some input.
+//     Make a function named isMultipleOfFive(input)
+// Make a function named isThree(input)
+// Make a function named isMultipleOfThree(input)
+// Make a function named isMultipleOfThreeAndFive(input)
+// Write a function named lowerCase(string)
+// Write a function named capitalizeEachWord(string) uses capitalize to return a string where each new word (separated by a space) is capitalized.
+//     Write a function named add(a, b) that returns the sum of a and b
+// Write a function named subtract(a, b) that returns a minus b
+// Write a function named multiply(a, b) that returns the product of a times b
+// Write a function named divide(a, b) that returns a divided by b
+// Write a function named remainder(a, b) that returns the remainder after dividing a by b
+// Write a function named average(arrayOfNumbers) that returns the average of an array of numbers and numeric strings.
--- JS/promises.js
+++ JS/promises.js
@@ -1,20 +1,27 @@
 "use strict";
 
 // function wait(number) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve();
-//         }, {});
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, number);
 //     });
-// };
-const wait = delay => {
-    return new Promise((resolve) => {
+// }
+
+
+const wait = (delay) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(delay);
+            // resolve(delay);
+           resolve("Delay finished after " + delay + " seconds");
         }, delay);
     });
-
 };
+
+wait(3000).then((data) => console.log(data));
+
+// let waitDelayPromise = wait();
+//     console.log(waitDelayPromise);
+//     waitDelayPromise.then((delay) => console.log('Delay wait done after $(delay / 1000) seconds'));
+
 
 //Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 //
@@ -22,18 +29,18 @@ const wait = delay => {
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-           const wait = delay => {
-               return new Promise((resolve) => {
-                   setTimeout(() => {
-                       resolve(delay);
-                   }, delay);
-               });
-           };
-// const waitDelayPromise = wait();
-//     console.log(waitDelayPromise);
-//     waitDelayPromise.then((delay) => console.log('Delay wait done after $(delay / 1000) seconds'));
-
-           wait(3000).then((delay) => console.log(`Wait finished after ${delay / 1000} seconds!`));
+//            const wait = (delay) => {
+//                return new Promise((resolve) => {
+//                    setTimeout(() => {
+//                        resolve(delay);
+//                    }, delay);
+//                });
+//            };
+// // let waitDelayPromise = wait();
+// //     console.log(waitDelayPromise);
+// //     waitDelayPromise.then((delay) => console.log('Delay wait done after $(delay / 1000) seconds'));
+//
+//            wait(3000).then((delay) => console.log(`Wait finished after ${delay / 1000} seconds!`));
 
 
 //Generate a Personal Access Token for the github api.
@@ -43,6 +50,32 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 //
 //fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
 //Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
+
+
+
+// function lastCommitDate(username) {
+//     let timeOfDates = [];
+//     const address = `https://api.github.com/users/${username}/events/public`;
+//     const authorToken = {
+//         headers: {
+//             'Authorization': 'token 9fe608508a84e43ec59f5f01ca3f4db7fa877ef1'
+//         }
+//     };
+//
+//     return fetch(address, authorToken)
+//         .then(response => response.json())
+//         .then(repositories => {
+//             respositories.map(repo => {
+//                 timeOfDates.push(repo.updated_at);
+//             });
+//             timeOfDates.sort();
+//             document.innerText = new Date(timeOfDates.length-1));
+//         });
+// }
+//
+// lastCommitDate("ClamsCasin0");
+
+
 const dateOfLastCommit = username => {
     const url = `https://api.github.com/users/${username}/events/public`;
 

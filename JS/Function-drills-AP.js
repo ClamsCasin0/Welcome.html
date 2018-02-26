51ea1d3236ff9666946cce11a0b4d8dd08cfae91
armandosegura <segura_arm@yahoo.com>
2018-02-26 09:11:22 -0600

    monday push

--- JS/Function-drills-AP.js
+++ JS/Function-drills-AP.js
@@ -13,6 +13,7 @@ console.log(sayHello("AJ"));
 function capitalize(string) {
     return string.toUpperCase().charAt(0);
 
+
 }
 
 //     Write a function named upperCase(string)
@@ -44,10 +45,36 @@ function isBoolean(input) {
     return typeof input === "boolean";
 }
 // Make a function named isTrue(boolean)
+function isTrue(boolean) {
+    return boolean == "true";
+}
 // Make a function named isFalse(boolean)
+function isFalse(boolean) {
+    return boolean == "false";
+}
 // Make a function named isTruthy(input)
+function isTruthy(input) {
+
+}
 // Make a function named isFalsy(input)
 // Make a function named isVowel(letter)
+function isVowel(letter) {
+    if(letter == "a") {
+        return true;
+    }
+    else if(letter == "e") {
+        return true;
+    } else if(letter == "i") {
+        return true;
+    } else if(letter == "o") {
+        return true;
+    } else if(letter = "u") {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 // Make a function named hasVowels(string) that uses isVowel to check if a string has any vowels
 // Make a function named isConsonant(letter)
 // Make a function named hasConsonants(string)
@@ -64,10 +91,12 @@ function isZero(number) {
 // Make a function named increment(number) that returns a number plus one
 function increment(number) {
     return number + 1;
+    //or return number +=;
 }
 // Make a function named decrement(number) that returns a number minus one
 function decrement(number) {
     return number - 1;
+    //or return number -=;
 }
 // Make a function named isFive(input)
 function isFive(input) {
@@ -76,6 +105,7 @@ function isFive(input) {
 // Make a function named addFive(input) that adds five to some input.
 function addFive(input) {
     return input + 5;
+    //or return input += 5;
 }
 //     Make a function named isMultipleOfFive(input)
 function isMultipleOfFive(input) {
@@ -87,12 +117,44 @@ function isThree(input) {
 
 }
 // Make a function named isMultipleOfThree(input)
+function isMultipleOfThree(input) {
+    return input % 3 == 0;
+}
 // Make a function named isMultipleOfThreeAndFive(input)
+function isMultipleOfThreeAndFive(input) {
+    return input % 3 == 0 && input % 5 == 0;
+}
 // Write a function named lowerCase(string)
+function lowerCase(string) {
+    return string.toLowerCase();
+}
 // Write a function named capitalizeEachWord(string) uses capitalize to return a string where each new word (separated by a space) is capitalized.
 //     Write a function named add(a, b) that returns the sum of a and b
+function add(a, b) {
+    return a + b;
+}
 // Write a function named subtract(a, b) that returns a minus b
+function subtract(a, b) {
+    return a - b;
+}
 // Write a function named multiply(a, b) that returns the product of a times b
+function multiply(a,b) {
+    return a * b;
+}
 // Write a function named divide(a, b) that returns a divided by b
+function divide(a, b) {
+    return a / b;
+}
 // Write a function named remainder(a, b) that returns the remainder after dividing a by b
+function remainder(a, b) {
+    return a % b;
+}
 // Write a function named average(arrayOfNumbers) that returns the average of an array of numbers and numeric strings.
+function average(arrayOfNumbers) {
+    var total = 0;
+    for(var i = 0; i < arrayOfNumbers.length; i++) {
+        total += arrayOfNumbers[i];
+    }
+    var avg = total / arrayOfNumbers.length;
+    return avg;
+    }
